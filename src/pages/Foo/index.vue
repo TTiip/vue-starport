53ac27f2ba5333db50605c3a3c5fdc8598a96d48
TTiip <2711430287@qq.com>
2022-05-06 22:20:02 +0800

    feat: 项目初始化

---
+++ src/pages/Foo/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>Foo</div>
+    <button
+      class="m-3 text-sm btn"
+      @click="goBack"
+    >
+      goBack
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const router = useRouter()
+  const goBack = () => { router.go(-1) }
+</script>
